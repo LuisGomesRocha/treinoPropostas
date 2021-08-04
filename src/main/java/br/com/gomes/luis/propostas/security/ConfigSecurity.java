cbcdfde71872474a0ca6aae39f10c8c6861cc995
LuisGomesRocha <luis.rocha@zup.com.br>
2021-08-04 18:41:59 -0300

    Login via senha cartão

--- src/main/java/br/com/gomes/luis/propostas/security/ConfigSecurity.java
+++ src/main/java/br/com/gomes/luis/propostas/security/ConfigSecurity.java
@@ -1,2 +1,25 @@
-package br.com.gomes.luis.propostas.security;public class ConfigSecurity {
+package br.com.gomes.luis.propostas.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class ConfigSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/proposta").hasAuthority("SCOPE_proposta-scope:read")
+                .antMatchers(HttpMethod.POST, "/proposta/**").hasAuthority("SCOPE_proposta-scope:write")
+                .anyRequest().permitAll()
+                .and().csrf().disable()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
 }
